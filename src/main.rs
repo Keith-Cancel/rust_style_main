92ae5b31dc1155d60f372d29208567d7444dfee7
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-08-12 16:05:28 -0700

    add some simple linux system calls.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
 #![no_std]
 
-fn main() {}
+fn main() {
+    stdout(b"Hello: from main\n");
+}
+
+// ==== Items needed for no_std ====
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    exit(101);
+}
+
+// ==== Some basic linux sys calls ====
+
+fn exit(code: i32) -> ! {
+    unsafe {
+        core::arch::asm!(
+            "syscall",
+            in("rdi") code,
+            in("rax") 60,
+            options(preserves_flags, nostack, noreturn),
+        );
+    }
+}
+
+fn stdout(buff: &[u8]) -> isize {
+    let mut ret: isize = 1;
+    let ptr = buff.as_ptr();
+    let len = buff.len();
+    unsafe {
+        core::arch::asm!(
+            "syscall",
+            in("rdi") 0,
+            in("rsi") ptr,
+            in("rdx") len,
+            inout("rax") ret,
+            out("r11") _, // syscall clobbers both rcx and r11
+            out("rcx") _,
+            options(preserves_flags, nostack),
+        );
+    }
+    return ret;
+}
