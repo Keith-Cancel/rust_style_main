43908f5436d1bf24cbd64bdb9c10c0170d4157ad
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-08-12 16:07:25 -0700

    Define termination and impl on some types.

--- lang_start/lib.rs
+++ lang_start/lib.rs
@@ -1,3 +1,35 @@
 #![no_std]
 #![feature(lang_items, never_type)]
 #![allow(internal_features)]
+
+#[lang = "termination"]
+pub trait Termination {
+    fn report(self) -> i32;
+}
+
+impl Termination for () {
+    fn report(self) -> i32 {
+        0
+    }
+}
+
+impl Termination for ! {
+    fn report(self) -> i32 {
+        unreachable!()
+    }
+}
+
+impl Termination for i32 {
+    fn report(self) -> i32 {
+        self
+    }
+}
+
+impl<T, E> Termination for Result<T, E> {
+    fn report(self) -> i32 {
+        match self {
+            Ok(_) => 0,
+            Err(_) => 101,
+        }
+    }
+}
